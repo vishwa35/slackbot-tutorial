d4f0d87fb7e69c6e39f53040f8e072707992cc6f
Vishwa Shah <vshah0350@gmail.com>
2018-06-12 00:38:47 -0700

    scheduled

---
+++ scheduled.py
@@ -0,0 +1,38 @@
+import os
+import schedule
+import time
+import logging
+from slackclient import SlackClient
+
+logging.basicConfig(level=logging.DEBUG)
+
+def sendMessage(slack_client):
+  msg = "Good Morning!"
+
+  # make the POST request through the python slack client
+  updateMsg = slack_client.api_call(
+    "chat.postMessage",
+    channel='#test',
+    text=msg
+  )
+
+  # check if the request was a success
+  if updateMsg['ok'] is not True:
+    logging.error(updateMsg)
+  else:
+    logging.debug(updateMsg)
+
+if __name__ == "__main__":
+  SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
+  slack_client = SlackClient(SLACK_BOT_TOKEN)
+  logging.debug("authorized slack client")
+
+  # # For testing
+  schedule.every(60).seconds.do(lambda: sendMessage(slack_client))
+
+  # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client))
+  logging.info("entering loop")
+
+  while True:
+    schedule.run_pending()
+    time.sleep(5) # sleep for 5 seconds between checks on the scheduler
